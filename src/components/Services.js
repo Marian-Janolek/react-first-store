6eb11dfd8b31387d601e19d55747666acbe6f0c6
Marian-Janolek <87386235+Marian-Janolek@users.noreply.github.com>
2021-08-16 17:22:02 +0200

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -54,8 +54,8 @@ const Wrapper = styled.section`
     padding: 2.5rem 2rem;
     border-radius: var(--radius);
     transition: all 0.5s ease-in-out;
-    -webkit-box-shadow: 2px 2px 7px 4px rgba(0,0,0,0.43); 
-    box-shadow: 2px 2px 7px 4px rgba(0,0,0,0.50);
+    -webkit-box-shadow: 2px 2px 7px 3px rgba(0,0,0,0.43); 
+    box-shadow: 2px 2px 7px 3px rgba(0,0,0,0.50);
     p {
       color: var(--clr-primary-2);
     }
